c2e3572a2c6b03201507d030f6df2f90e5d275b8
Willian Augusto Soder de Souza <williansoder@gmail.com>
2024-07-14 15:20:20 -0300

    Create main.py

---
+++ CARD - 2/main.py
@@ -0,0 +1,20 @@
+'''Todas execucoes e chamadas de funcao deixei dentro de cada arquivo especifico'''
+
+
+#import variaveis
+#import basicos
+#import listas
+#import tuplas
+#import conjuntos 
+#import dicionario
+#import operadores
+#import controle
+#import funcoesbasico
+#import args
+#import funcional
+#import map_reduce
+#import lambdas 
+#import comprehension
+#import POO
+#import POOheranca
+#import POOmembros
