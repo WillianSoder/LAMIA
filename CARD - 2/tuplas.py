db56076b2e231a7cdb56113da5037fef6e6672a6
Willian Augusto Soder de Souza <williansoder@gmail.com>
2024-07-14 15:20:43 -0300

    Create tuplas.py

---
+++ CARD - 2/tuplas.py
@@ -0,0 +1,6 @@
+# Explorando o tipo tupla em python
+nomes = ('willian', 'joao', 'maria') # Criação de um tupla
+print(type(nomes)) # 'tuple'
+print(len(nomes))
+print('willian' in nomes)
+print(nomes[1:2])
