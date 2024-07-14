695189a397bd42ccc3d0ca81c6a4d490127eb3e3
Willian Augusto Soder de Souza <williansoder@gmail.com>
2024-07-14 15:20:46 -0300

    Create variaveis.py

---
+++ CARD - 2/variaveis.py
@@ -0,0 +1,19 @@
+a = 5
+b = 6
+
+print(a + b)
+
+texto = 'seu nome é: '
+nome = 'Willian'
+
+print(texto + nome)
+
+print(5 * 'olá' )
+
+pi = 3.1415
+raio = float(input('Digite o raio:'))
+
+area = pi * raio * raio 
+
+print(f'a area é: {area}m^2')
+
