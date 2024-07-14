c580330d66a84eb0d2f75e5a9e0662511d288ede
Willian Augusto Soder de Souza <williansoder@gmail.com>
2024-07-14 15:20:17 -0300

    Create listas.py

---
+++ CARD - 2/listas.py
@@ -0,0 +1,12 @@
+# Explorando a classe list em python 
+numeros = [10, 20, 30]
+print(type(numeros)) # 'list'
+
+numeros.append(30) # anexa valor 30 a lista 
+print(len(numeros)) # tamnanho da lista
+
+numeros[3] = -54
+
+numeros.insert(0,77) # insere 77 no inicio
+print(numeros)
+
