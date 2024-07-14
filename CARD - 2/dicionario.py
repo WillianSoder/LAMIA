e9bc575c7132ae6a1625d508ccc7a996dbbdc307
Willian Augusto Soder de Souza <williansoder@gmail.com>
2024-07-14 15:19:45 -0300

    Create dicionario.py

---
+++ CARD - 2/dicionario.py
@@ -0,0 +1,11 @@
+# Explorando o tipo dicionario em python 
+aluno = {
+    'nome': 'willian',
+    'nota': 9.8,
+    'ativo': False
+}  # Criando o dicionario 
+print(type(aluno)) # 'dict'
+print(len(aluno))
+print(aluno['nome'])
+print(aluno['nota'])
+print(aluno['ativo'])
